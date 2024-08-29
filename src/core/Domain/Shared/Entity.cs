708b9260e174728ca4e9ca8c3d9d714795d71041
Irakli_Imesashvili <imesashviliirakli5@gmail.com>
2024-08-30 03:09:49 +0400

    Add project files.

---
+++ src/core/Domain/Shared/Entity.cs
@@ -0,0 +1,31 @@
+﻿namespace Domain.Shared;
+
+internal abstract class Entity
+{
+    public Guid Id { get; protected set; }
+
+    private readonly List<DomainEvent> _domainEvents = new List<DomainEvent>();
+    public IReadOnlyCollection<DomainEvent> DomainEvents => _domainEvents.AsReadOnly();
+
+    protected void AddDomainEvent(DomainEvent domainEvent)
+    {
+        _domainEvents.Add(domainEvent);
+    }
+
+    public void RemoveDomainEvent(DomainEvent domainEvent)
+    {
+        _domainEvents.Remove(domainEvent);
+    }
+
+    public void ClearDomainEvents()
+    {
+        _domainEvents.Clear();
+    }
+
+    // Equality checks based on identity
+    public override bool Equals(object obj) =>
+        obj is Entity entity && Id == entity.Id;
+
+    public override int GetHashCode() =>
+        Id.GetHashCode();
+}
